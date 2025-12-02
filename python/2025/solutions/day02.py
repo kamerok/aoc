495590cbf1be86d8c0a7d0aba6147c229a421d4f
Maksim Dvoretskii <kamerok@gmail.com>
2025-12-02 20:05:56 +0100

    Solve Day 2 2025 Part 2

--- python/2025/solutions/day02.py
+++ python/2025/solutions/day02.py
@@ -4,16 +4,16 @@ from utils.utils import read_input, read_test_input, check
 
 def part_1(data):
     ranges = list(map(lambda raw_range: raw_range.split('-'), data[0].split(',')))
-    return sum(sum([find_symmetrical_matches(start, end) for start, end in ranges], []))
+    return sum(sum([find_symmetrical_matches(start, end, 2) for start, end in ranges], []))
 
 
-def find_symmetrical_matches(start, end):
-    if len(start) % 2 > 0:
-        start = pow(10, len(start))
+def find_symmetrical_matches(start, end, base):
+    if (mod := len(start) % base) > 0:
+        start = pow(10, len(start) - 1 + (base - mod))
     else:
         start = int(start)
-    if len(end) % 2 > 0:
-        end = pow(10, (len(end) - 1)) - 1
+    if (mod := len(end) % base) > 0:
+        end = pow(10, (len(end) - 1)) - mod
     else:
         end = int(end)
 
@@ -21,17 +21,24 @@ def find_symmetrical_matches(start, end):
         return []
 
     result = []
-    half_start = int(f'{start}'[0:int(len(f'{start}')/2)])
-    half_end = int(f'{end}'[0:int(len(f'{end}')/2)])
-    for half in range(half_start, half_end + 1):
-        number = int(f'{half}{half}')
+    part_start = int(f'{start}'[0:int(len(f'{start}') / base)])
+    part_end = int(f'{end}'[0:int(len(f'{end}') / base)])
+    for part in range(part_start, part_end + 1):
+        number = int(f'{part}' * base)
         if number in range(start, end + 1):
             result.append(number)
     return result
 
 
 def part_2(data):
-    pass
+    ranges = list(map(lambda raw_range: raw_range.split('-'), data[0].split(',')))
+    result = 0
+    for start, end in ranges:
+        matches = []
+        for base in range(2, max(len(start), len(end)) + 1):
+            matches += find_symmetrical_matches(start, end, base)
+        result += sum(set(matches))
+    return result
 
 
 sample_data = read_test_input(2025, 2)
