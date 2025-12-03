dc66239704b33885175d1d0ba0bb9ceb242ce27a
Maksim Dvoretskii <kamerok@gmail.com>
2025-12-03 06:51:09 +0100

    Solve Day 3 2025 Part 2

--- python/2025/solutions/day03.py
+++ python/2025/solutions/day03.py
@@ -3,24 +3,24 @@ from utils.utils import read_input, read_test_input, check
 
 
 def part_1(data):
-    return sum(map(find_number, data))
+    return sum(map(lambda line: find_number(line, 2), data))
 
 
-def find_number(line):
-    first = -1
-    second = -1
+def find_number(line, base):
+    numbers = [-1] * base
     for i, c in enumerate(line):
-        number = int(c)
-        if number > first and i < (len(line) - 1):
-            first = number
-            second = -1
-        elif number > second:
-            second = number
-    return int(f'{first}{second}')
+        candidate = int(c)
+        for position, number in enumerate(numbers):
+            if candidate > number and i < len(line) - (base - position - 1):
+                numbers[position] = candidate
+                for j in range(position + 1, base):
+                    numbers[j] = -1
+                break
+    return int(''.join(map(str, numbers)))
 
 
 def part_2(data):
-    pass
+    return sum(map(lambda line: find_number(line, 12), data))
 
 
 sample_data = read_test_input(2025, 3)
